d6ff4af66eeb28264f9953e4289e3034ef1e125a
TamNT2 <tamnt2@edoctor.vn>
2021-07-14 15:43:57 +0700

    create & update post

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const URL = 'https://dev-culi.herokuapp.com/';
+const URL = 'https://dev-culi.herokuapp.com';
 
 export const fetchPosts = () => axios.get(`${URL}/posts`);
 export const createPost = (payload) => axios.post(`${URL}/posts`, payload);
